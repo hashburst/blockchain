b9f0cf5a18af315e3f02538ee27a1d2aaf7841db
Gabriele Pegoraro <55950175+hashburst@users.noreply.github.com>
2024-09-24 15:47:11 +0200

    Update sample.php

--- evm/API/PHP/sample.php
+++ evm/API/PHP/sample.php
@@ -1,12 +1,12 @@
 <?php
 
-// Esempio di chiamata
-$cryptoType = 'BTC';
-$toAddress = '1A1zP1eP5QGe***************Na';
-$amount = 10000;  // In satoshi (BTC) o unità specifica per altre criptovalute
-$apiKey = 'YOUR_BLOCKCYPHER_API_KEY';
-
-$response = sendCryptoPayment($cryptoType, $toAddress, $amount, $apiKey);
-print_r($response);
+  // Esempio di chiamata
+  $cryptoType = 'BTC';
+  $toAddress = '1A1zP1eP5QGe***************Na';
+  $amount = 10000;  // In satoshi (BTC) o unità specifica per altre criptovalute
+  $apiKey = 'YOUR_BLOCKCYPHER_API_KEY';
+  
+  $response = sendCryptoPayment($cryptoType, $toAddress, $amount, $apiKey);
+  print_r($response);
 
 ?>
