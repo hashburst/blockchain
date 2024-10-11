efda3ccdc0f0c3a169ec53343a020236cdcc5f1e
Gabriele Pegoraro <55950175+hashburst@users.noreply.github.com>
2024-10-11 19:26:00 +0200

    Update blockchain.go

--- GO/blockchain.go
+++ GO/blockchain.go
@@ -7,7 +7,7 @@ type Blockchain struct {
 	Blocks []*Block
 }
 
-// AddBlock adds a new block to the chain using PoH consensus
+/* AddBlock adds a new block to the chain using PoH consensus
 func (bc *Blockchain) AddBlock(data string) {
 	latestBlock := bc.Blocks[len(bc.Blocks)-1]
 	newBlock := NewBlock(data, latestBlock.Hash, PoH(latestBlock.ProofOfTime))
@@ -17,6 +17,28 @@ func (bc *Blockchain) AddBlock(data string) {
 		log.Println("Block validation failed")
 	}
 }
+*/
+func (bc *Blockchain) AddBlock(mempool *Mempool) {
+    transactions := mempool.GetTransactions()
+    if len(transactions) == 0 {
+        log.Println("No transactions available in the Mempool.")
+        return
+    }
+
+    latestBlock := bc.Blocks[len(bc.Blocks)-1]
+    newBlock := NewBlock(transactions, latestBlock.Hash, PoH(latestBlock.ProofOfTime))
+    if bc.ValidateBlock(newBlock) {
+        bc.Blocks = append(bc.Blocks, newBlock)
+        // Rimuovere le transazioni incluse nel nuovo blocco dalla Mempool
+        for _, tx := range transactions {
+            mempool.RemoveTransaction(tx.ID)
+        }
+        log.Println("New block added to the blockchain.")
+    } else {
+        log.Println("Block validation failed.")
+    }
+}
+
 
 // ValidateBlock checks if the block is valid
 func (bc *Blockchain) ValidateBlock(newBlock *Block) bool {
