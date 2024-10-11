47695915885b7739b889e73682ad1d095a9c074f
Gabriele Pegoraro <55950175+hashburst@users.noreply.github.com>
2024-10-11 18:00:14 +0200

    Create messaging.py

---
+++ hashburstNetworksBuilderFramework/communications/messaging.py
@@ -0,0 +1,27 @@
+# File: ./communications/messaging.py
+import socket
+import threading
+
+class ReticulumMessaging:
+    def __init__(self, host='127.0.0.1', port=65432):
+        self.host = host
+        self.port = port
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def start_server(self):
+        self.server_socket.bind((self.host, self.port))
+        self.server_socket.listen()
+        print(f"Server started on {self.host}:{self.port}")
+
+        while True:
+            conn, addr = self.server_socket.accept()
+            threading.Thread(target=self.handle_client, args=(conn, addr)).start()
+
+    def handle_client(self, conn, addr):
+        print(f"Connection from {addr}")
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            print(f"Received: {data.decode()}")
+            conn.sendall(data)
