8d1d2228d13e78c978f48fbf5a466e16921c6906
Gabriele Pegoraro <55950175+hashburst@users.noreply.github.com>
2024-10-11 18:11:18 +0200

    Update __init__.py

--- hashburstNetworksBuilderFramework/__init__.py
+++ hashburstNetworksBuilderFramework/__init__.py
@@ -1,7 +1,2 @@
 # File: hashburstNetworksBuilderFramework/__init__.py
 # This file makes the hashburstNetworksBuilderFramework a package
-
-# File: hashburstNetworksBuilderFramework/blockchain/__init__.py
-from .mining import HashburstMining
-from .minting import HashburstMinting
-from .evm import HashburstEVM
