da29832418c0d9c5f42269790b4fb1231e0f4cdf
Gabriele Pegoraro <55950175+hashburst@users.noreply.github.com>
2024-10-11 17:57:03 +0200

    Create minting.py

---
+++ hashburstNetworksBuilderFramework/blockchain/minting.py
@@ -0,0 +1,8 @@
+# File: /blockchain/minting.py
+class HashburstMinting:
+    def __init__(self, initial_supply=1000000):
+        self.supply = initial_supply
+
+    def mint_tokens(self, amount: int):
+        self.supply += amount
+        return self.supply
