23d130f3ee81867236491c85c89857e10793b7f9
Gabriele Pegoraro <55950175+hashburst@users.noreply.github.com>
2024-10-11 17:57:48 +0200

    Create evm.py

---
+++ hashburstNetworksBuilderFramework/blockchain/evm.py
@@ -0,0 +1,12 @@
+# File: /blockchain/evm.py
+class HashburstEVM:
+    def __init__(self):
+        self.contracts = {}
+
+    def deploy_contract(self, contract_name: str, bytecode: str):
+        self.contracts[contract_name] = bytecode
+        return f"Contract {contract_name} deployed"
+
+    def call_contract(self, contract_name: str, function_name: str, *args):
+        # Simplified representation of invoking a contract
+        return f"Called {function_name} on {contract_name} with arguments {args}"
